ddd0f0b4a9f5d8631158957664a2315e8e4cf57d
ramazan-shira <ramazanshira1@gmail.com>
2024-04-18 14:32:28 +0200

    Widgets added

--- script.js
+++ script.js
@@ -12,10 +12,140 @@ window.onscroll = () => {
   navbar.classList.remove("open");
 };
 
+//Chart
+var dataset = [
+  { name: "Programming", percent: 34.1 },
+  { name: "Cyber Security", percent: 27.7 },
+  { name: "Design", percent: 24.5 },
+  { name: "Digital marketing", percent: 13.7 },
+];
+
+var pie = d3.layout
+  .pie()
+  .value(function (d) {
+    return d.percent;
+  })
+  .sort(null)
+  .padAngle(0.03);
+
+var w = 250,
+  h = 250;
+
+var outerRadius = w / 2;
+var innerRadius = 80;
+
+var color = d3.scale.category10();
+
+var arc = d3.svg.arc().outerRadius(outerRadius).innerRadius(innerRadius);
+
+var svg = d3
+  .select("#chart")
+  .append("svg")
+  .attr({
+    width: w,
+    height: h,
+    class: "shadow",
+  })
+  .append("g")
+  .attr({
+    transform: "translate(" + w / 2 + "," + h / 2 + ")",
+  });
+var path = svg
+  .selectAll("path")
+  .data(pie(dataset))
+  .enter()
+  .append("path")
+  .attr({
+    d: arc,
+    fill: function (d, i) {
+      return color(d.data.name);
+    },
+  });
+
+path
+  .transition()
+  .duration(1000)
+  .attrTween("d", function (d) {
+    var interpolate = d3.interpolate({ startAngle: 0, endAngle: 0 }, d);
+    return function (t) {
+      return arc(interpolate(t));
+    };
+  });
+
+var restOfTheData = function () {
+  var text = svg
+    .selectAll("text")
+    .data(pie(dataset))
+    .enter()
+    .append("text")
+    .transition()
+    .duration(200)
+    .attr("transform", function (d) {
+      return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr("dy", ".2em")
+    .attr("text-anchor", "middle")
+    .text(function (d) {
+      return d.data.percent + "%";
+    })
+    .style({
+      fill: "#fff",
+      "font-size": "10px",
+    });
+
+  var legendRectSize = 20;
+  var legendSpacing = 7;
+  var legendHeight = legendRectSize + legendSpacing;
+
+  var legend = svg
+    .selectAll(".legend")
+    .data(color.domain())
+    .enter()
+    .append("g")
+    .attr({
+      class: "legend",
+      transform: function (d, i) {
+        //Just a calculation for x & y position
+        return "translate(-35," + (i * legendHeight - 65) + ")";
+      },
+    });
+  legend
+    .append("rect")
+    .attr({
+      width: legendRectSize,
+      height: legendRectSize,
+      rx: 20,
+      ry: 20,
+    })
+    .style({
+      fill: color,
+      stroke: color,
+    });
+
+  legend
+    .append("text")
+    .attr({
+      x: 20,
+      y: 15,
+    })
+    .text(function (d) {
+      return d;
+    })
+    .style({
+      fill: "#237",
+      "font-size": "12px",
+    });
+};
+
+setTimeout(restOfTheData, 1000);
+
 // Admission form validation
 var fullName = document.getElementById("full-name");
 var emailAddress = document.getElementById("email-address");
 var phone = document.getElementById("phone");
+
+var gender = document.querySelectorAll('.input.gender input[type="radio"]');
+
 var female = document.getElementById("female");
 var male = document.getElementById("male");
 var course = document.getElementById("course");
@@ -23,10 +153,10 @@ var selectedCourse = course.selectedIndex;
 var selectedCourseValue = course.options[selectedCourse].value;
 var selectedCourseText = course.options[selectedCourse].text;
 
-var java = document.getElementById("java");
-var net = document.getElementById("net");
-var javascript = document.getElementById("javascript");
-var react = document.getElementById("react");
+var knowledge = document.querySelectorAll(
+  '.input-item.coding input[type="checkbox"]'
+);
+
 var password = document.getElementById("password");
 var confirmPassword = document.getElementById("confirm-password");
 
@@ -124,9 +254,15 @@ const register = () => {
   }
 
   //Validate gender
-  if (!female.checked && !male.checked) {
-    genderError.innerText = "Please select your gender!";
+  var genderChecked = false;
+  for (var i = 0; i < gender.length; i++) {
+    if (gender[i].checked) {
+      genderChecked = true;
+    }
+  }
+  if (!genderChecked) {
     valid = false;
+    genderError.innerText = "Please select your gender";
   } else {
     valid = true;
     genderError.innerText = "";
@@ -147,9 +283,16 @@ const register = () => {
   }
 
   //Validate knowledge
-  if (!java.checked && !net.checked && !javascript.checked && !react.checked) {
-    knowledgeError.innerText = "Please choose a programming language";
+  var anyCheckboxChecked = false;
+  for (var i = 0; i < knowledge.length; i++) {
+    if (knowledge[i].checked) {
+      anyCheckboxChecked = true; // Një checkbox është zgjedhur
+    }
+  }
+
+  if (!anyCheckboxChecked) {
     valid = false;
+    knowledgeError.innerText = "Please choose a programming language";
   } else {
     valid = true;
     knowledgeError.innerText = "";
@@ -205,7 +348,7 @@ clearBtn.addEventListener("click", function (e) {
   female.checked = false;
   male.checked = false;
   course.selectedIndex = 0;
-  fileInput.value = "";
+  // fileInput.value = "";
   java.checked = false;
   net.checked = false;
   javascript.checked = false;
