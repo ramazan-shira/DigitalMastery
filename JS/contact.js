cc1e84f38fcc5d5c197bafb20dbc175b73aca6be
ramazan-shira <ramazanshira1@gmail.com>
2024-04-20 14:32:27 +0200

    Form validation

--- JS/contact.js
+++ JS/contact.js
@@ -6,18 +6,43 @@ const submit = () => {
   const email = document.getElementById("email").value.trim();
   const contactMessage = document.getElementById(" contact-message");
 
-  const regEx = /^[a-zA-Z]+$/;
+  const errorFname = document.getElementById("error-fname");
+  const errorLname = document.getElementById("error-fname");
+  const errorEmail = document.getElementById("error-email");
+  const errorMessage = document.getElementById("error-message");
 
-  if (fname === "" || lname === "" || email === "" || message === "") {
-    alert("Please fill out all fileds");
-  } else if (!regEx.test(fname)) {
-    alert("First name should contain only letters!");
+  const regEx = /^[a-zA-Z]+$/;
+  var valid = true;
+
+  if (!fname) {
+    errorFname.innerText = "Please enter your first name";
+    valid = false;
   } else if (fname.length < 3) {
-    alert("First name should contain at least 3 characters");
-  } else if (!regEx.test(lname)) {
-    alert("Last name should contain only letters!");
-  } else if (lname.length < 3) {
-    alert("Last name should contain at least 3 characters");
+    errorFname.innerText = "Last name must be at least 3 characters";
+    valid = false;
+  } else if (regEx.test(fname)) {
+    errorFname.innerText = "Last name  must contain only letters!";
+    valid = false;
+  } else{
+    valid = true;
+    errorFname.innerText = "";
+  }
+
+  if (!lname) {
+    errorLname.innerText = "Please enter your last name";
+    valid = false;
+  } else if (fname.length < 3) {
+    errorLname.innerText = "Last name must be at least 3 characters";
+    valid = false;
+  } else if (regEx.test(fname)) {
+    errorLname.innerText = "Last name  must contain only letters!";
+    valid = false;
+  } else{
+    valid = true;
+    errorLname.innerText = "";
+  }
+
+
   } else {
     var messageContact = document.createElement("p");
     messageContact.innerHTML = `Hello ${fname} ${lname}, we have received your message. Thank you for reaching out to us.`;
